f59e45906913314cb77c9c8b600305e7f3c2bcdb
Sergio Salguero <salgueroflor1982@gmail.com>
2021-07-16 16:02:32 +0200

    create screens HOme and Product

--- original/frontend/src/App.js
+++ original/frontend/src/App.js
@@ -1,60 +1,31 @@
 import React from "react";
-import data from "./data";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Original
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img
-                  className="medium"
-                  src={product.image}
-                  alt={product.name}
-                ></img>
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                </div>
-                <div className="price">{product.price}€</div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              TOB
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route exact path="/" component={HomeScreen}></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- original/frontend/src/data.js
+++ original/frontend/src/data.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 const data = {
   products: [
     {
@@ -7,6 +6,7 @@ const data = {
       category: "Shirts",
       image: "/images/p1.jpeg",
       price: 120,
+      countInStock: 10,
       brand: "Nike",
       rating: 4.5,
       numReviews: 10,
@@ -18,6 +18,7 @@ const data = {
       category: "Shirts",
       image: "/images/p2.jpeg",
       price: 100,
+      countInStock: 20,
       brand: "Adidas",
       rating: 4,
       numReviews: 10,
@@ -29,6 +30,7 @@ const data = {
       category: "Shirts",
       image: "/images/p3.jpeg",
       price: 220,
+      countInStock: 0,
       brand: "Lacoste",
       rating: 4.8,
       numReviews: 17,
@@ -40,6 +42,7 @@ const data = {
       category: "Pants",
       image: "/images/p4.jpeg",
       price: 78,
+      countInStock: 15,
       brand: "Nike",
       rating: 4.5,
       numReviews: 14,
@@ -51,6 +54,7 @@ const data = {
       category: "Pants",
       image: "/images/p5.jpeg",
       price: 65,
+      countInStock: 5,
       brand: "Puma",
       rating: 4.5,
       numReviews: 10,
@@ -62,6 +66,7 @@ const data = {
       category: "Pants",
       image: "/images/p6.jpeg",
       price: 139,
+      countInStock: 12,
       brand: "Adidas",
       rating: 4.5,
       numReviews: 15,
