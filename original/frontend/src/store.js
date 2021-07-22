093bc9faa561423a118992bbb30c14114bbfad8a
Sergio Salguero <salgueroflor1982@gmail.com>
2021-07-22 10:44:28 +0200

    actions and reducers(redux)

--- original/frontend/src/store.js
+++ original/frontend/src/store.js
@@ -1,11 +1,11 @@
-import data from "./data";
-import { applyMiddleware, compose, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
+import { productListReducers } from "./reducers/productReducers";
 
 const initialState = {};
-const reducer = (state, action) => {
-  return { products: data.products };
-};
+const reducer = combineReducers({
+  productList: productListReducers,
+});
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
