bdcb8aefc2d6166cc5657f278bb05db3cc0b8653
Sergio Salguero <salgueroflor1982@gmail.com>
2021-08-13 21:43:35 +0200

    checkout card

--- original/frontend/src/screens/CartScreen.js
+++ original/frontend/src/screens/CartScreen.js
@@ -17,8 +17,13 @@ export default function CartScreen(props) {
       dispatch(addToCart(productId, qty));
     }
   }, [dispatch, productId, qty]);
+
   const removeFromCartHandler = (id) => {
     //delete Action
+  };
+
+  const checkoutHandler = () => {
+    props.history.push("/signin?redirect=shipping");
   };
   return (
     <div className="row top">
@@ -48,8 +53,7 @@ export default function CartScreen(props) {
                       value={item.qty}
                       onChange={(e) =>
                         dispatch(
-                          addToCart(item.product),
-                          Number(e.target.value)
+                          addToCart(item.product, Number(e.target.value))
                         )
                       }
                     >
@@ -74,6 +78,26 @@ export default function CartScreen(props) {
             ))}
           </ul>
         )}
+      </div>
+      <div className="col-1">
+        <div className="card card-body">
+          <ul>
+            <li>
+              <h2>
+                Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) :{" "}
+                {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}€
+              </h2>
+            </li>
+            <button
+              type="button"
+              onClick={checkoutHandler}
+              className="primary block"
+              disabled={cartItems === 0}
+            >
+              Proceed to Checkout
+            </button>
+          </ul>
+        </div>
       </div>
     </div>
   );
