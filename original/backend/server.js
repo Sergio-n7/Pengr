010d764fd48e59b1899cc5254d48b85844d043eb
Sergio Salguero <salgueroflor1982@gmail.com>
2021-08-22 16:32:14 +0200

    Created Place order API

--- original/backend/server.js
+++ original/backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
 const app = express();
@@ -20,6 +21,7 @@ mongoose.connect(
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
