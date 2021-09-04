382bf8e2e23e40e5a5ed19a4d8c6c8837a584ae1
Sergio Salguero <salgueroflor1982@gmail.com>
2021-09-04 19:20:45 +0200

    update profile route

--- original/frontend/src/actions/userAction.js
+++ original/frontend/src/actions/userAction.js
@@ -12,7 +12,6 @@ import {
   USER_SIGNOUT,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
-  USER_UPDATE_PROFILE_RESET,
   USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants";
 
@@ -86,7 +85,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     userSignin: { userInfo },
   } = getState();
   try {
-    const { data } = await axios.updateUserProfile(`/api/users/profile`, user, {
+    const { data } = await axios.put(`/api/users/profile`, user, {
       headers: { Authorization: `Bearer ${userInfo.token}` },
     });
     dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
